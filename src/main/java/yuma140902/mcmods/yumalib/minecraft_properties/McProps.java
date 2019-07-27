b3b241645ad8b1e25266d3fe04e98c44607d8267
yuma <yuma140902@gmail.com>
2019-07-27 22:11:09 +0900

    Add McProps

---
+++ src/main/java/yuma140902/mcmods/yumalib/minecraft_properties/McProps.java
@@ -0,0 +1,29 @@
+package yuma140902.mcmods.yumalib.minecraft_properties;
+
+public final class McProps {
+	
+	public static final int
+		PLANK_META_OAK = 0,
+		PLANK_META_ACACIA = 4,
+		PLANK_META_BIRCH = 2,
+		PLANK_META_DARKOAK = 5,
+		PLANK_META_JUNGLE = 3,
+		PLANK_META_SPRUCE = 1;
+	
+	public static final int
+		LOG_META_OAK = 0,
+		LOG_META_SPRUCE = 1,
+		LOG_META_BIRCH = 2,
+		LOG_META_JUNGLE = 3,
+		LOG2_META_ACACIA = 0,
+		LOG2_META_DARK_OAK = 1;
+	
+	public static final int
+		SIDE_BOTTOM = 0,
+		SIDE_TOP = 1,
+		SIDE_NORTH = 2,
+		SIDE_SOUTH = 3,
+		SIDE_WEST = 4,
+		SIDE_EAST = 5;
+	
+}
