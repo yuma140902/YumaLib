682219c328eeda7079f8d41fb6cdb4b13adb5ae4
yuma <yuma140902@gmail.com>
2019-07-28 09:08:26 +0900

    Add YumaLibApi

---
+++ src/main/java/yuma140902/mcmods/yumalib/YumaLibApi.java
@@ -0,0 +1,39 @@
+package yuma140902.mcmods.yumalib;
+
+import yuma140902.mcmods.yumalib.registry.GenericRegistry;
+import yuma140902.mcmods.yumalib.registry.IReadOnlyIdRegistry;
+import yuma140902.mcmods.yumalib.registry.IReadOnlyRegistry;
+import yuma140902.mcmods.yumalib.registry.IdRegistry;
+import yuma140902.mcmods.yumalib.sounds.INoteBlockInstrument;
+import yuma140902.mcmods.yumalib.updatecheck.IUpdateChecker;
+
+public class YumaLibApi {
+	
+	public static String getVersion() {
+		return YumaLib.MOD_VERSION;
+	}
+	
+	private static GenericRegistry<IUpdateChecker> updateNotifierRegistry = new GenericRegistry<>();
+	
+	/**
+	 * Register UpdateCheckers here to use update notification on logging in to the world
+	 * @param updateChecker
+	 */
+	public static void registerUpdateChecker(IUpdateChecker updateChecker) {
+		updateNotifierRegistry.register(updateChecker);
+	}
+	
+	public static IReadOnlyRegistry<IUpdateChecker> getUpdateNotifierRegistry(){
+		return updateNotifierRegistry;
+	}
+	
+	private static IdRegistry<INoteBlockInstrument> noteBlockInstrumentReg = new IdRegistry<>();
+	
+	public static IReadOnlyIdRegistry<INoteBlockInstrument> getNoteBlockInstrumentRegistry(){
+		return noteBlockInstrumentReg;
+	}
+	
+	public static void registerNoteBlockInstrument(INoteBlockInstrument noteBlockInstrument) {
+		noteBlockInstrumentReg.register(noteBlockInstrument);
+	}
+}
