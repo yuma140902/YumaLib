202c52d589b350eb052d302514899ec2042cc08c
yuma <yuma140902@gmail.com>
2019-07-28 09:30:27 +0900

    かまど精錬時の経験値の定数を追加

---
+++ src/main/java/yuma140902/mcmods/yumalib/recipe/RecipeStat.java
@@ -0,0 +1,12 @@
+package yuma140902.mcmods.yumalib.recipe;
+
+public class RecipeStat {
+	public static final float
+		SMELTING_XP_FOR_STONE = 0.1F,
+		SMELTING_XP_FOR_LOG = 0.15F,
+		SMELTING_XP_FOR_CACTUS = 0.2F,
+		SMELTING_XP_FOR_CLAY = 0.3F,
+		SMELTING_XP_FOR_FOOD = 0.35F,
+		SMELTING_XP_FOR_ORE = 0.7F,
+		SMELTING_XP_FOR_RARE_ORE = 1.0F;
+}
